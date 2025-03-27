ffd82a78468b09700d5052216141debee07d46b9
tontsa28 <miika@tontsa.fi>
2025-03-27 20:53:16 +0200

    feat: add the ability to log into an account

--- app.py
+++ app.py
@@ -1,37 +1,53 @@
-from flask import Flask
-from flask import render_template, request, flash, redirect
+import secrets
 import sqlite3
 
+from flask import Flask, flash, redirect, render_template, request, session
+
+import config
 import users
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index() -> str:
     return render_template("index.html")
 
-@app.route("/login")
-def login() -> str:
-    return render_template("login.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-@app.route("/register")
-def register() -> str:
-    return render_template("register.html")
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            session["username"] = username
+            session["csrf_token"] = secrets.token_hex(16)
+            return redirect("/")
+        else:
+            flash("ERROR: invalid username or password")
+            return redirect("/login")
+    else:
+        return render_template("login.html")
 
-@app.route("/create", methods=["POST"])
-def create():
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
 
-    if password1 != password2:
-        flash("ERROR: salasanat eivät täsmää")
-        return redirect("/register")
-    
-    try:
-        users.create_user(username, password1)
-    except sqlite3.IntegrityError:
-        flash("ERROR: tunnus on jo varattu")
-        return redirect("/register")
+        if password1 != password2:
+            flash("ERROR: salasanat eivät täsmää")
+            return redirect("/register")
+        
+        try:
+            users.register_user(username, password1)
+        except sqlite3.IntegrityError:
+            flash("ERROR: tunnus on jo varattu")
+            return redirect("/register")
 
-    return redirect("/")
+        return redirect("/")
+    else:
+        return render_template("register.html")

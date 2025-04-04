6d0f716f92123afa7501cf575372aedcb30eeffb
tontsa28 <miika@tontsa.fi>
2025-04-04 15:27:12 +0300

    feat: expand database schema to include aircraft

--- schema.sql
+++ schema.sql
@@ -3,3 +3,21 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password TEXT
 );
+
+CREATE TABLE aircraft (
+    id INTEGER PRIMARY KEY,
+    manufacturer TEXT,
+    model TEXT,
+    registration TEXT,
+    category TEXT,
+    airline TEXT,
+    times_onboard INTEGER,
+    times_seen INTEGER,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE images (
+    id INTEGER PRIMARY KEY,
+    aircraft_id INTEGER REFERENCES aircraft,
+    image BLOB
+);

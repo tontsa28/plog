33a4cedbc6c313386334843be259a5c2e0422e14
tontsa28 <miika@tontsa.fi>
2025-04-10 16:35:51 +0300

    feat: add current values to item edit form

--- items.py
+++ items.py
@@ -10,13 +10,40 @@ def remove_item(item_id: int) -> None:
     db.execute(sql, [item_id])
 
 def get_items() -> list:
-    sql = "SELECT A.id, A.airline, A.manufacturer, A.model, A.registration, U.username FROM aircraft A LEFT JOIN users U ON A.user_id = U.id"
+    sql = """SELECT A.id,
+                    A.manufacturer,
+                    A.model,
+                    A.registration,
+                    A.airline,
+                    A.user_id,
+                    U.username
+            FROM aircraft A, users U
+            WHERE A.user_id = U.id"""
     return db.query(sql)
 
 def get_item(item_id: int) -> list | None:
-    sql = "SELECT A.id, A.airline, A.manufacturer, A.model, A.registration, A.user_id, U.username FROM aircraft A LEFT JOIN users U ON A.user_id = U.id WHERE A.id = ?"
+    sql = """SELECT A.id,
+                    A.manufacturer,
+                    A.model,
+                    A.registration,
+                    A.category,
+                    A.airline,
+                    A.times_onboard,
+                    A.times_seen,
+                    A.user_id,
+                    U.username
+            FROM aircraft A, users U
+            WHERE A.user_id = U.id AND A.id = ?"""
     return db.query_one(sql, [item_id])
 
 def update_item(item_id: int, manufacturer: str, model: str, registration: str, category: str, airline: str, times_onboard: int, times_seen: int) -> None:
-    sql = "UPDATE aircraft SET manufacturer = ?, model = ?, registration = ?, category = ?, airline = ?, times_onboard = ?, times_seen = ? WHERE id = ?"
+    sql = """UPDATE aircraft
+            SET manufacturer = ?,
+                model = ?,
+                registration = ?,
+                category = ?,
+                airline = ?,
+                times_onboard = ?,
+                times_seen = ?
+            WHERE id = ?"""
     db.execute(sql, [manufacturer, model, registration, category, airline, times_onboard, times_seen, item_id])
